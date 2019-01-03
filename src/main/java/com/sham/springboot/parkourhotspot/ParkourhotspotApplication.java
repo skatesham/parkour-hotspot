067577100b78356a7af1e760b69858a733612590
skatesham <sham.vincius@gmail.com>
2019-01-03 20:46:51 -0200

    spring boot initializer

---
+++ src/main/java/com/sham/springboot/parkourhotspot/ParkourhotspotApplication.java
@@ -0,0 +1,14 @@
+package com.sham.springboot.parkourhotspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkourhotspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkourhotspotApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/sham/springboot/parkourhotspot/ParkourhotspotApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sham.springboot.parkourhotspot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParkourhotspotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
